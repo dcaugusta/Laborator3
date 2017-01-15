c7dc9ab321de102a0832f707b0b12519dcb30b58
Augusta Dunca <dcaugusta@gmail.com>
2017-01-15 10:41:46 +0200

    Exercitiul 1- racheta cu temporizare

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-
+#include <windows.h>  //  Pentru a putea apela Sleep()
 using namespace std;
 
 int main()
 {
   for ( int i= 10 ; i>0; i--) {
     cout <<i<< endl;
+    Sleep(1000); // temporizare 1sec
     }
     cout << "racheta lansata " << endl;
     return (0);
