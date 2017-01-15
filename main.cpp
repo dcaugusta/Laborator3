b955db8371cc9e25e0ca75bdca21a153c50f4f34
Augusta Dunca <dcaugusta@gmail.com>
2017-01-15 11:02:13 +0200

    Exercitiul 2- sir 1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-int a[20],i;
-    for (i=0;i<20;i=i++) {
-        a[i]=i+1;
-    }
-    for (i=0;i<20;i++){
-        cout<< a[i]<< " ";
-    }
-    cout << endl;
-    return (0);
+int main(){
+   int a[20], i;
+   for (i = 0; i < 20; i++) {
+      a[i] = i + 1;
+   }
+   for (i = 0; i < 20; i++) {
+      cout << a[i] << " ";
+   }
+   cout << endl;
+   return 0;
 }
